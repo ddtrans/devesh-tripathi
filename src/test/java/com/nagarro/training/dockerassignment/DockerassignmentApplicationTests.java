969101f3e58da8e30ec5067df4c471cff02f00a4
deveshtripathi <devesh.tripathi@nagarro.com>
2023-03-14 16:20:04 +0530

    removed extra directory

---
+++ src/test/java/com/nagarro/training/dockerassignment/DockerassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.training.dockerassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
